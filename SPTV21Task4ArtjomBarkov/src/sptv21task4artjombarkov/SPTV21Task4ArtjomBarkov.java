9aa958d2717cc4dddbff35ed92559e9845d37876
ArtjomBarkov <112948103+ArtjomBarkov@users.noreply.github.com>
2022-09-27 11:09:34 +0300

    Add files via upload

---
+++ SPTV21Task4ArtjomBarkov/src/sptv21task4artjombarkov/SPTV21Task4ArtjomBarkov.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptv21task4artjombarkov;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ *
+ * @author user
+ */
+public class SPTV21Task4ArtjomBarkov {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            Random random = new Random();
+        int n = 101;
+        int sum = 0;
+        System.out.println("----------Одномерные Массивы-----------");
+        int[] nums = new int[n];
+        for (int i = 0; i < nums.length; i++) {
+            while(true){
+                int rundNum = random.nextInt(100);
+                if (rundNum % 2 != 0){
+                    nums[i] = rundNum;
+                    break;
+                }
+            }
+        }
+        Arrays.sort(nums);    
+        for (int i = 1; i < nums.length; i++) {
+            System.out.printf("%-4d", nums[i]);
+            sum += nums[i];  
+            if (i%10==0){
+                System.out.println("");
+            }     
+        }
+        System.out.println();
+        System.out.println("Среднее арифметическое: " + sum/n);
+    }
+    
+}
